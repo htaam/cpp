74a40e56dc7d944f72c51d65ab4e3612a1ada811
Tiago <matias90tiago@gmail.com>
2022-12-23 02:17:02 +0000

    cpp07 done

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,68 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template<typename T>
+class Array{
+
+public:
+    
+    Array<T>(void):the_size(0), array(NULL){
+        std::cout << "Empty constructor called" << std::endl;
+    }
+    
+    Array<T>( const unsigned int size): the_size(size){
+        std::cout << "Size constructor called" << std::endl;
+        if (size < 0)
+            throw std::overflow_error("Invalid size");
+        this->array = new T[size];
+    }
+    
+    Array<T>( const Array<T> &og ){
+		std::cout << "Copy constructor called" << std::endl;
+		*this = og;
+	}
+    
+    ~Array<T>( void ){
+		std::cout << "Destructor called" << std::endl;
+		if (this->the_size > 0)
+			delete [] this->array;
+	}
+
+    Array<T> &operator=(const Array<T> &og){
+        std::cout << "Assignment overload called" << std::endl;
+        if (this == &og)
+            return (*this);
+        /*if (this->size > 0){
+           delete [] this->array;
+        }*/
+        this->the_size = og.size();
+        this->array = new T[this->the_size];
+        for (int i = 0; i < this->the_size; i++)
+         this->array[i] = og[i];
+        return *this;
+    }
+
+    T &operator[](const int index) const{
+        if (index >= this->the_size || index < 0)
+            throw std::overflow_error("Index out of bounds");
+        return this->array[index];
+    }
+
+    int size() const{
+        return this->the_size;
+    }
+
+private:
+    int the_size;
+    T   *array;
+
+
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &ostr, const Array<T> &instance);
+
+
+#endif
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include "Array.hpp"
+#include <cstdlib>
+
+
+#define MAX_VAL 750
+int main(int, char**)
+{
+    {
+        Array<int> numbers(MAX_VAL);
+        int* mirror = new int[MAX_VAL];
+        srand(time(NULL));
+        for (int i = 0; i < MAX_VAL; i++)
+        {
+            const int value = rand();
+            numbers[i] = value;
+            mirror[i] = value;
+        }
+        //SCOPE
+        std::cout << "SCOPE" << std::endl;
+        {
+            Array<int> tmp = numbers;
+            Array<int> test(tmp);
+        }
+        std::cout << "End SCOPE" << std::endl;
+        for (int i = 0; i < MAX_VAL; i++)
+        {
+            if (mirror[i] != numbers[i])
+            {
+                std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+        }
+        try
+        {
+            numbers[-2] = 0;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        try
+        {
+            numbers[MAX_VAL] = 0;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+
+        delete [] mirror;//
+    }
+    {
+        Array<int>			intArray;
+        Array<std::string>	stringArray(10);
+        Array<std::string>	otherStringArray;
+
+        stringArray[0] = "Hi";
+        stringArray[1] = "Hello";
+        stringArray[2] = "How are you?";
+        stringArray[3] = "Fine and you?";
+        stringArray[4] = "Doing great thanks!";
+        stringArray[5] = "You're very welcome";
+        std::cout << "intArray.size() = " << intArray.size() << std::endl;
+        std::cout << "stringArray.size() = " << stringArray.size() << std::endl;
+        otherStringArray = stringArray;
+        otherStringArray[3] = "Go away!!!";
+        std::cout << stringArray[3] << std::endl;
+        std::cout << otherStringArray[3] << std::endl;
+
+        try
+        {
+            std::cout << stringArray[59] << std::endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+
+        try
+        {
+            std::cout << stringArray[-3] << std::endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+    }
+}
